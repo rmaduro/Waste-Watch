37a410f20ad37041e1f0fbe7a034aab2bd21d6a4
Rodrigo Maduro <rodrigomaduro.pro@gmail.com>
2025-02-01 17:14:54 +0000

    logo and icons

--- waste-watch/src/app/components/side-nav/side-nav.component.ts
+++ waste-watch/src/app/components/side-nav/side-nav.component.ts
@@ -7,22 +7,21 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
   template: `
-    <div class="bg-dark text-white" style="width: 250px; min-height: 100vh;">
+  <div class="bg-white text-white border-end" style="width: 100px; min-height: 100vh; border-right: 5px solid #757575;">
+
       <div class="p-3">
-        <img src="assets/logo.png" alt="WasteWatch Logo" class="img-fluid mb-4">
+        <img src="assets/images/logo2.png" alt="WasteWatch Logo" class="img-fluid mb-4">
         <ul class="nav flex-column">
           <li class="nav-item">
-            <a class="nav-link text-white" routerLink="/bin-dashboard" routerLinkActive="active">
-              <i class="bi bi-trash"></i> Bin Management
-            </a>
+            <a class="nav-link text-dark" routerLink="/bin-dashboard" routerLinkActive="active">
+              <i class="bi bi-trash"></i></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white" routerLink="/fleet-dashboard" routerLinkActive="active">
-              <i class="bi bi-truck"></i> Fleet Management
-            </a>
+            <a class="nav-link text-dark" routerLink="/fleet-dashboard" routerLinkActive="active">
+              <i class="bi bi-truck"></i></a>
           </li>
           <li class="nav-item mt-auto">
-            <a class="nav-link text-white" (click)="logout()">
+            <a class="nav-link text-dark" (click)="logout()">
               <i class="bi bi-box-arrow-right"></i> Logout
             </a>
           </li>
